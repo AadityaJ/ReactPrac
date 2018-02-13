0ad66eb95ce3e78784e35426fc00942043577ac5
AadityaJ <aadityajamuar@gmail.com>
2018-02-13 19:44:25 +0530

    adding

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {
+  Component,
+} from 'react';
+import { connect } from 'react-redux';
 
-class App extends Component {
-  render() {
-    return (
-        <div>
-            Basic page
-        </div>
+import {
+	getData,
+} from './redux';
+
+// App.js
+export class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			text:'',
+			loc:'tweets'
+		};
+		this.handleChange = this.handleChange.bind(this)
+	}
+	componentWillMount() {
+    this.props.getData()  // Uncomment when onLoad is used
+  }
+	handleChange(event){
+		this.props.getData(this.state.loc)
+	}
+  	render() {
+    	return (
+      	<div >
+      		<button > Get me </button>
+			{(this.props.data != undefined)?console.log(this.props.data["2"].text):''}
+      	</div>
     );
-    }
+  }
 }
-export default App;
+
+// AppContainer.js
+const mapStateToProps = (state) => ({
+	data: state.data,
+});
+
+const mapDispatchToProps = {
+	// final: (comments)=>finalText(comments),
+	getData: (id)=>getData(id),
+};
+
+const AppContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+export default AppContainer;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { Switch, BrowserRouter, Route } from 'react-router-dom'
-import MainApp from './CityClock';
-const ShortBengaluru = ({ match }) => {
-    return <MainApp cityName="Bengaluru" offset={match.params.num} x={match.params.x} y={match.params.y}/>
-}
-const ShortSLC = ({ match }) => {
-    return <MainApp cityName="SLC" offset={match.params.num}x={match.params.x} y={match.params.y}/>
-}
-const ShortNYC = ({ match }) => {
-    return <MainApp cityName="NYC" offset={match.params.num}x={match.params.x} y={match.params.y}/>
-}
-ReactDOM.render((
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={App}/>
-            <Route path="/bengaluru/:num/x/:x/y/:y" component={ShortBengaluru}/>
-            <Route path="/slc/:num/x/:x/y/:y" component={ShortSLC}/>
-            <Route path="/nyc/:num/x/:x/y/:y" component={ShortNYC}/>
-        </Switch>
-    </BrowserRouter>
-), document.getElementById('root')
+import './index.css';
+
+// Add these imports - Step 1
+import { Provider } from 'react-redux';
+import { store } from './redux';
+
+// Wrap existing app in Provider - Step 2
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
-registerServiceWorker();
